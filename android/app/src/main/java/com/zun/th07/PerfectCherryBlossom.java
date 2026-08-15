ccb21ccafe8e0fa2d6e4f6d82d4db9ab735cfca5
Swiizyu <swiizyu@gmail.com>
2026-08-16 01:25:33 +0300

    touhou

--- android/app/src/main/java/com/zun/th07/PerfectCherryBlossom.java
+++
@@ -1,9 +0,0 @@
-package com.zun.th07;
-
-import org.libsdl.app.SDLActivity;
-
-/**
- * A sample wrapper class that just calls SDLActivity
- */
-
-public class PerfectCherryBlossom extends SDLActivity { }
